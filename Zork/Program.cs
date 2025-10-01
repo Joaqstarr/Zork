6dca167cc4a0c825d28562c32e266df30727d085
Joaquin Royer <joaqstarr@gmail.com>
2025-10-01 14:56:53 -0400

    Created Zork game loop

--- Zork/Program.cs
+++ Zork/Program.cs
@@ -6,11 +6,53 @@ using System.Threading.Tasks;
 
 namespace Zork
 {
+    enum Commands
+    {
+        QUIT,
+        LOOK,
+        NORTH,
+        SOUTH,
+        EAST,
+        WEST,
+        UNKNOWN
+    }
+
+
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Commands command = Commands.UNKNOWN;
+            while (command != Commands.QUIT)
+            {
+                Console.Write("> ");
+                command = ToCommand(Console.ReadLine().Trim());
+
+                string outputString = "";
+                switch (command)
+                {
+                    case Commands.LOOK:
+                        outputString = "A rubber mat saying 'Welcome to Zork!' lies by the door.";
+                        break; 
+                    case Commands.NORTH:
+                    case Commands.SOUTH:
+                    case Commands.EAST:
+                    case Commands.WEST:
+                           outputString = "You moved " + command.ToString() + ".";
+                        break;
+                    case Commands.QUIT:
+                        outputString = "Exiting the game.";
+                        break;
+                    default:
+                        outputString = "Unknown command.";
+                        break;
+                }
+
+                Console.WriteLine(outputString);
+            }
         }
+
+
+        public static Commands ToCommand(string commandString) => Enum.TryParse(commandString, true, out Commands command) ? command : Commands.UNKNOWN;
     }
 }
